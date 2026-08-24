f775a555662f23207c22f92ac5f38b04c30cd15a
jayson <jayson@saavylab.com>
2026-08-24 14:36:14 -0600

    make remote shell test portable

--- server/collectors/__tests__/ssh.test.js
+++ server/collectors/__tests__/ssh.test.js
@@ -1,18 +1,26 @@
 import test from "node:test";
 import assert from "node:assert/strict";
 import { execFileSync } from "node:child_process";
+import { existsSync } from "node:fs";
 import { remoteBashCommand } from "../ssh.js";
 
-test("remote commands use Bash even when the login shell is Fish", () => {
-  const command = [
-    "value=amd",
-    "case \"$value\" in amd) echo gpu-ok;; *) echo wrong;; esac",
-    "for item in one two; do printf ':%s' \"$item\"; done",
-  ].join("; ");
+const command = [
+  "value=amd",
+  "case \"$value\" in amd) echo gpu-ok;; *) echo wrong;; esac",
+  "for item in one two; do printf ':%s' \"$item\"; done",
+].join("; ");
 
-  const output = execFileSync("fish", ["-c", remoteBashCommand(command)], {
+test("remote command wrapper round-trips Bash syntax through a login shell", () => {
+  const output = execFileSync("sh", ["-c", remoteBashCommand(command)], {
     encoding: "utf8",
   });
+  assert.equal(output, "gpu-ok\n:one:two");
+});
 
+const fish = ["/usr/bin/fish", "/usr/local/bin/fish"].find(existsSync);
+test("remote commands use Bash when the login shell is Fish", { skip: !fish }, () => {
+  const output = execFileSync(fish, ["-c", remoteBashCommand(command)], {
+    encoding: "utf8",
+  });
   assert.equal(output, "gpu-ok\n:one:two");
 });
